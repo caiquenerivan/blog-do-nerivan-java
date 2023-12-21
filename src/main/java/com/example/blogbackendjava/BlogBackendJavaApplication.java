5cb1a5846b28b58bec3d4ad77681b34ee4a7c432
caiquenerivan <caiquenerivansantos@gmail.com>
2023-12-21 15:53:59 -0300

    Começo do projeto

---
+++ src/main/java/com/example/blogbackendjava/BlogBackendJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.blogbackendjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogBackendJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogBackendJavaApplication.class, args);
+	}
+
+}
